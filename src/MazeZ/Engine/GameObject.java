d2daa80ecd4bb88be032c72112ec303bf45accbc
Kulitapa Taumoepeau <Tapa@Kulitapas-MacBook-Pro.local>
2016-07-25 17:27:11 -0600

    add Game Object and test

---
+++ src/MazeZ/Engine/GameObject.java
@@ -0,0 +1,34 @@
+package MazeZ.Engine;
+
+import MazeZ.Graphics.Position;
+
+/**
+ * Created by Tapa on 7/25/16.
+ */
+public class GameObject
+{
+    protected Position mPos;
+
+    public GameObject()
+    {
+        this(new Position());
+    }
+
+    public GameObject(Position pos)
+    {
+        mPos = pos;
+    }
+
+    public Position getPosition() {
+        return mPos;
+    }
+
+    public void setPosition(Position pos) {
+        this.mPos = pos;
+    }
+
+    public void move(Position pos)
+    {
+        mPos.add(pos);
+    }
+}
