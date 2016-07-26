63df12a5ab653b340f4c956e6cf0ce3a0f9e9ab6
Rhayme <Rhayme00@gmail.com>
2016-07-25 18:34:05 -0600

    Implement Entity class

--- src/MazeZ/Engine/Entity.java
+++ src/MazeZ/Engine/Entity.java
@@ -1,8 +1,67 @@
 package MazeZ.Engine;
 
+
+
+import MazeZ.Graphics.Drawable;
+import MazeZ.Graphics.Position;
+import MazeZ.Graphics.RenderWindow;
+
 /**
  * Created by lukit on 7/25/2016.
  */
-public class Entity
+public class Entity extends GameObject implements Drawable
 {
+	protected int mHealth;
+	protected char mSymbol;
+
+	public Entity()
+	{
+		this('?', 100);
+	}
+
+	public Entity(char sym, int health)
+	{
+		health = mHealth;
+		mSymbol = sym;
+
+	}
+
+	public Entity(char sym, int health, Position pos)
+	{
+		super(pos);
+		health = mHealth;
+		mSymbol = sym;
+		
+	}
+
+	public boolean isAlive()
+	{
+		return false;
+
+	}
+
+	public void setHealth(int health)
+	{
+		mHealth = health;
+	}
+
+	public int getHealth()
+	{
+		return mHealth;
+
+	}
+
+	public char getSymbol()
+	{
+
+		return mSymbol;
+
+	}
+
+	@Override
+	public void draw(RenderWindow target)
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
