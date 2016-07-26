355c9aa8ff20cb6623e1488880b7069c31d699d1
Edward Jenco <lukitree@gmail.com>
2016-07-25 23:05:15 -0600

    Implement Config class

--- src/MazeZ/Engine/Config.java
+++ src/MazeZ/Engine/Config.java
@@ -1,5 +1,8 @@
 package MazeZ.Engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lukit on 7/25/2016.
  */
@@ -12,7 +15,32 @@ public class Config
 		return ourInstance;
 	}
 
+	private List<Map> maps;
+	private int mCurrentLevel;
+	private int mDeathCount;
+
 	private Config()
 	{
+		mCurrentLevel = 0;
+
+		maps = new ArrayList<>();
+
+		// Load map files
+		maps.add(new Map("maps/MazeEddy.txt"));
+		maps.add(new Map("maps/TestMaze.txt"));
+	}
+
+	public void nextLevel()
+	{
+		++mCurrentLevel;
+		if(mCurrentLevel == maps.size())
+		{
+			--mCurrentLevel;
+		}
+	}
+
+	public Map getMap()
+	{
+		return maps.get(mCurrentLevel);
 	}
 }
